ec99b638c882578e58fe0b6845f8983df93eff3d
Saivon Schenck <schencksaivon@gmail.com>
2022-08-21 03:15:45 -0400

    Practiced Switch Statements

--- switch-statements.js
+++ switch-statements.js
@@ -1,1 +1,22 @@
 //can a case be a conditional?
+
+function message(percentage) {
+
+
+let printMessage = ""
+switch(percentage) {
+    case 50:
+        printMessage = "half way there bruv.";
+    break;
+    case 100:
+        printMessage = "You are on you way out mate!"
+    break;
+    default:
+        printMessage = "keep fighting on"
+}
+
+console.log(printMessage)
+
+}
+
+message(100)
