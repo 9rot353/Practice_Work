0de18a4a63d8ee9b4adb421b8cfa8f3fd1f64985
Saivon Schenck <schencksaivon@gmail.com>
2022-12-13 14:18:15 -0500

    Large Repo update

---
+++ Practice Folders/December-13-2022_practice.js
@@ -0,0 +1,27 @@
+function grabDoll (dolls) {
+    let bag = []
+
+    for (let i = 0; i < dolls.length; i++) {
+        let doll = dolls[i]
+
+        if (doll == "Hello Kitty" || doll == "Barbie doll") {
+            bag.push(doll)
+        }else if (doll != "Hello Kitty" || doll != "Barbie doll") {
+            continue
+        }
+
+        if (bag.length >= 3){
+            break
+        }
+    }
+return bag 
+}
+
+let set = ["Mickey Mouse","Barbie doll","Hello Kitty","Hello Kitty","Hello Kitty","Snow white"]
+console.log(grabDoll(set))
+
+/* Create for loop
+if arr[i] = "Hello Kitty" or "Barbie doll" add it to the bag.
+else arr != "Hello Kitty" or Barbie doll continue.
+if bag.length > 3 break.
+*/
