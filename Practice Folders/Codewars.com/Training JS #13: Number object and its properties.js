10d55e1ca5f4e16ae928eee6be8cedbac190341e
Saivon Schenck <schencksaivon@gmail.com>
2022-12-15 21:12:23 -0500

    Second Solution to CodeWars : https://www.codewars.com/kata/5722fd3ab7162a3a4500031f/solutions/javascript

--- Practice Folders/Codewars.com/Training JS #13: Number object and its properties.js
+++ Practice Folders/Codewars.com/Training JS #13: Number object and its properties.js
@@ -10,27 +10,33 @@ let grandElse = `Input number is ${n}`
 
 if(isNaN(n)) return "Input number is Number.NaN"
 
-switch(n) {
-    case Number.MAX_VALUE:
-         return possibleResults[0];
-    break;
-    case Number.MIN_VALUE:
-        return possibleResults[1];
-    case Number.NaN:
-        return possibleResults[2];
-    case Number.NEGATIVE_INFINITY:
-        return possibleResults[3];
-    case Number.POSITIVE_INFINITY :
-        return possibleResults[4];
-    default :
-    return grandElse;
-}
 
-}
+return n == Number.MAX_VALUE ? possibleResults[0]
+        : n == Number.MIN_VALUE ? possibleResults[1]
+        : n == Number.NEGATIVE_INFINITY ? possibleResults[3]
+        : n == Number.POSITIVE_INFINITY ? possibleResults[4]
+        : grandElse;
+        
+
+// switch(n) {
+//     case Number.MAX_VALUE:
+//          return possibleResults[0];
+//     break;
+//     case Number.MIN_VALUE:
+//         return possibleResults[1];
+//     case Number.NEGATIVE_INFINITY:
+//         return possibleResults[3];
+//     case Number.POSITIVE_INFINITY :
+//         return possibleResults[4];
+//     default :
+//     return grandElse;
+// }
+
+ }
 
 
 
-let test = whatNumberIsIt(NaN)
+let test = whatNumberIsIt(Number.POSITIVE_INFINITY)
 console.log(test)
 
 /*
@@ -40,5 +46,6 @@ create condition statments judge the n input,
 Store all the number values into their own variable. 
 use ternary operaters instead of if statements.
 
+Also create a solution using the Switch Statment.
 Solve this twice.
 */
